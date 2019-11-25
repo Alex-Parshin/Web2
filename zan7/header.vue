2b80dc64223e14eb5b6177eb9632d26264700c51
Alex-Parshin <55979959+Alex-Parshin@users.noreply.github.com>
2019-11-25 17:07:17 +0400

    t35t3b2

---
+++ zan7/header.vue
@@ -0,0 +1,15 @@
+<style scoped>
+  h1{
+    color: red;
+  }
+</style>
+<template>
+  module.export={
+    data: function() {
+      return {
+        msg: "hello world"
+      };
+    }
+  };
+
+</template>
---
+++ zan7/main.js
@@ -0,0 +1,8 @@
+var Vue =require("vue");
+var Header = require("./header.vue");
+var vm=new Vue ({
+  el: "body",
+  components:{
+    cTag:Header
+  }
+});
